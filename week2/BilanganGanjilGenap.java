dd1c99647ffd4f951ee396190de9829d2a8d1f0c
virgantara <vinux.edu@gmail.com>
2024-11-04 11:24:55 +0700

    week2

---
+++ week2/BilanganGanjilGenap.java
@@ -0,0 +1,16 @@
+public class BilanganGanjilGenap{
+	public static void main(String[] args){
+		int bilangan = 120;
+
+		// ini contoh komentar
+		if (bilangan % 2 == 0){
+			System.out.println("Genap");
+		}
+
+		else{
+			System.out.println("Ganjil");
+		}
+
+
+	}
+}
---
+++ week2/Looping.java
@@ -0,0 +1,21 @@
+public class Looping{
+	
+	// Looping adalah perulangan
+	public static void main(String [] args){
+		
+		// perulangan menggunakan while
+		int indeks = 1;
+		int angka = 1;
+
+		while(indeks <= 5){
+			angka = angka * indeks;
+			indeks = indeks + 1;
+
+			System.out.println("Angka: " + );
+			System.out.println(indeks);
+		}
+
+	}
+
+
+}
