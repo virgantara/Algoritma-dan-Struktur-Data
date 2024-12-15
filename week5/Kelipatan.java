9ad656294289febfb6e6fbfe30cbc2014a614d34
virgantara <oddy@unida.gontor.ac.id>
2024-12-15 18:37:19 +0700

    fizbuzz

---
+++ week5/Kelipatan.java
@@ -0,0 +1,29 @@
+
+public class Kelipatan{
+	public static void main(String []args){
+
+		int total = 15;
+
+		for(int i = 1 ;i<= total ;i++){
+
+			if(i % 3 == 0 && i % 5 ==0){
+				System.out.println("FizzBuzz");
+			}
+			else if (i % 3 == 0){
+				System.out.println("Fizz");	
+			}
+
+			else if(i % 5 == 0){
+				System.out.println("Buzz");
+			}
+
+			else{
+				System.out.println(i);
+			}
+			
+		}
+
+
+
+	}
+}
---
+++ week5/README.md
@@ -0,0 +1,8 @@
+# FizzBuzz 
+
+## Petunjuk:
+Write a program that:
+1. prints numbers from 1 to n (inclusive). 
+2. For multiples of 3, print "Fizz"; 
+3. For multiples of 5, print "Buzz"; 
+4. For multiples of both 3 and 5, print "FizzBuzz".
