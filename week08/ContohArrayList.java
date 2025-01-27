4723448c073e3aaa404b6c674b820e966d7bbdd1
virgantara <vinux.edu@gmail.com>
2025-01-27 11:00:46 +0700

    Update

---
+++ week08/ContohArrayList.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class ContohArrayList{
+
+	public static void main(String[]args){
+		ArrayList<Mahasiswa> daftar = new ArrayList<>();
+
+		Mahasiswa mhs1 = new Mahasiswa();
+		mhs1.nim = "123";
+		mhs1.nama = "Budi";
+
+		daftar.add(mhs1);	
+
+		System.out.println(daftar.size());
+	}
+
+}
+
+class Mahasiswa {
+	public String nim="";
+	public String nama="";
+
+	public String toString(){
+		return this.nim+" "+this.nama;
+	}
+}
